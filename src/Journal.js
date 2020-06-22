86d78683a1d87c3beb61922aaad5c23cd75f9b12
Ian Gregg <iangregg188@gmail.com>
2020-06-22 14:12:33 -0700

    it will now have updated JS business logic but still working on the kinks within the new format.
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- src/Journal.js
+++ src/Journal.js
@@ -1,7 +1,7 @@
 //Buiness Logic
-function wordsToNumber(){
+export function wordsToNumber(){
   let totalWords = 0;
-  for (var i=0; 1< Wordcount.length; i++){
+  for (var i=0; 1 < Wordcount.length; i++){
     if (String(i)) === " ") {
       totalWords = +1;
     }
@@ -9,11 +9,29 @@ function wordsToNumber(){
   }
 }
 
-function vowelsToConsonants(){
+export function vowelsToConsonants(){
+
 
 }
 
-function getTeaser(){
+export function isVowels(){
+  let vowels = ["a", "e", "i", "o", "u"]
+  let letters; 
+  for (v = 0; v < vowels.length; v++) {
+    if (vowels[v] === vowels)
+    vowels === true;
+  }
+}
+
+export function convertWordstoStrings (){
+  let inputSentence = inputSentence.split(" ")
+  let inputWord = inputSentence.split("")
+  
+
+
+}
+
+export function getTeaser(){
 
 }
 
